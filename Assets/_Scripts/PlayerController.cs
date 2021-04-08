48c8322ade60f7ecb33dbf468ff63dbe7a68b48a
gabrielztk <gabrielztk@al.insper.edu.br>
2021-04-07 22:19:44 -0300

    MAPA

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+
+
+// Referências para a imagem do player: https://www.gratispng.com/png-xlgs6i/
+
+
+
+public class PlayerController : MonoBehaviour
+
+{
+
+[Range(1, 10)]
+
+public float velocity;
+
+
+
+void Update()
+
+{
+
+float inputX = Input.GetAxis("Horizontal");
+
+float inputY = Input.GetAxis("Vertical");
+
+
+
+transform.position +=new Vector3(inputX, inputY, 0) * Time.deltaTime * velocity;
+
+}
+
+
+
+
+
+}
