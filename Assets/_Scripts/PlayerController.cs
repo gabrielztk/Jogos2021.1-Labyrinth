5d1a5d048d0dc03667cb1d504da1d4865df8b2f3
gabrielztk <gabrielztk@al.insper.edu.br>
2021-04-08 22:19:44 -0300

    Commit

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -4,38 +4,20 @@ using System.Collections.Generic;
 
 using UnityEngine;
 
-
-
 // Referências para a imagem do player: https://www.gratispng.com/png-xlgs6i/
-
-
-
 public class PlayerController : MonoBehaviour
-
 {
-
-[Range(1, 10)]
-
-public float velocity;
+    [Range(1, 10)]
+    public float velocity;
 
 
 
-void Update()
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
 
-{
-
-float inputX = Input.GetAxis("Horizontal");
-
-float inputY = Input.GetAxis("Vertical");
-
-
-
-transform.position +=new Vector3(inputX, inputY, 0) * Time.deltaTime * velocity;
-
-}
-
-
-
-
+        transform.position +=new Vector3(inputX, inputY, 0) * Time.deltaTime * velocity;
+    }
 
 }
